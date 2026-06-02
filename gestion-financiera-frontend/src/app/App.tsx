328966548a305ac518551969c7dcee93b200a3ac
marianamartinezgaviria1 <mariana.martinez.gaviria1@gmail.com>
2026-06-01 21:43:08 -0500

    fix: eliminar ping manual, UptimeRobot mantiene el backend despierto

--- gestion-financiera-frontend/src/app/App.tsx
+++ gestion-financiera-frontend/src/app/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
 import { AppProvider } from './context/AppContext';
@@ -6,12 +5,6 @@ import { ThemeProvider } from './context/ThemeContext';
 import { Toaster } from './components/ui/sonner';
 
 export default function App() {
-
-  useEffect(() => {
-  fetch('https://eko-gestion-financiera-backend.onrender.com/swagger-ui/index.html')
-    .catch(() => {});
-}, []);
-
   return (
     <ThemeProvider>
       <AppProvider>
